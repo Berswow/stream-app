c39a58854c3d8f1e7600e6dcdb7cc3c15b478dea
Hamzin M. <bersfromhell@gmail.com>
2025-04-04 06:53:32 +0200

    svg fixes

--- src/components/Home/Category.tsx
+++ src/components/Home/Category.tsx
@@ -60,7 +60,7 @@ export const Category = () => {
                         <span>
                         <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                <path fillRule="evenodd" clipRule="evenodd"
                                       d="M0.75 2.59479C0.75 0.930971 2.53383 -0.123757 3.9917 0.678068L17.4557 8.08328C18.9668 8.91436 18.9668 11.0856 17.4557 11.9167L3.9917 19.3219C2.53383 20.1238 0.75 19.069 0.75 17.4052V2.59479Z"
                                       fill="white"/>
                          </svg>
@@ -76,11 +76,11 @@ export const Category = () => {
                         ))}
                     </div>
                     <div className='flex gap-40 mt-2'>
-                        <p>Action</p>
+                        <p>Adventure</p>
                         <span>
                         <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                <path fillRule="evenodd" clipRule="evenodd"
                                       d="M0.75 2.59479C0.75 0.930971 2.53383 -0.123757 3.9917 0.678068L17.4557 8.08328C18.9668 8.91436 18.9668 11.0856 17.4557 11.9167L3.9917 19.3219C2.53383 20.1238 0.75 19.069 0.75 17.4052V2.59479Z"
                                       fill="white"/>
                          </svg>
@@ -96,11 +96,11 @@ export const Category = () => {
                         ))}
                     </div>
                     <div className='flex gap-40 mt-2'>
-                        <p>Action</p>
+                        <p>Comedy</p>
                         <span>
                         <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                <path fillRule="evenodd" clipRule="evenodd"
                                       d="M0.75 2.59479C0.75 0.930971 2.53383 -0.123757 3.9917 0.678068L17.4557 8.08328C18.9668 8.91436 18.9668 11.0856 17.4557 11.9167L3.9917 19.3219C2.53383 20.1238 0.75 19.069 0.75 17.4052V2.59479Z"
                                       fill="white"/>
                          </svg>
@@ -116,11 +116,11 @@ export const Category = () => {
                         ))}
                     </div>
                     <div className='flex gap-40 mt-2'>
-                        <p>Action</p>
+                        <p>Drama</p>
                         <span>
                         <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                <path fillRule="evenodd" clipRule="evenodd"
                                       d="M0.75 2.59479C0.75 0.930971 2.53383 -0.123757 3.9917 0.678068L17.4557 8.08328C18.9668 8.91436 18.9668 11.0856 17.4557 11.9167L3.9917 19.3219C2.53383 20.1238 0.75 19.069 0.75 17.4052V2.59479Z"
                                       fill="white"/>
                          </svg>
@@ -136,11 +136,11 @@ export const Category = () => {
                         ))}
                     </div>
                     <div className='flex gap-40 mt-2'>
-                        <p>Action</p>
+                        <p>Horror</p>
                         <span>
                         <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                <path fillRule="evenodd" clipRule="evenodd"
                                       d="M0.75 2.59479C0.75 0.930971 2.53383 -0.123757 3.9917 0.678068L17.4557 8.08328C18.9668 8.91436 18.9668 11.0856 17.4557 11.9167L3.9917 19.3219C2.53383 20.1238 0.75 19.069 0.75 17.4052V2.59479Z"
                                       fill="white"/>
                          </svg>
